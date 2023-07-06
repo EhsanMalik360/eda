5c3ac2fe7fb480de8150ec3b0e13111e8f359d94
EhsanMalik360 <leverify.bi022@gmail.com>
2023-07-06 11:13:06 +0500

    limits added

--- eda.py
+++ eda.py
@@ -15,7 +15,20 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
 
 st.title('EDA Tool')
 uploaded_file = st.file_uploader("Choose a file", type=['csv', 'xlsx'])
+st.markdown('<style>.eex3ynb0{visibility: hidden;}</style>', unsafe_allow_html=True)
 if uploaded_file is not None:
+
+    filename=uploaded_file.name
+
+    if(filename.endswith("xlsx")):
+        st.warning('Only CSV files are allowed. Please upgrade to Premium for other type of files', icon="⚠️")
+        exit(1)
+    filesize=uploaded_file.size
+
+
+    if (filesize>100000):
+        st.warning('File upload limit is 10 mb. Please upgrade to Premium for large files', icon="⚠️")
+        exit(1)
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
